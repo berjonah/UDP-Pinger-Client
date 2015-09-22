f26e986bb95fd2a2040c6cb2e51486a3511729d5
Andrew Fillmore <fillmoreandrew110@gmail.com>
2015-09-22 10:54:26 -0600

    Timeout fixed

--- UDPPingerClient/UDPPingerClient/UDPPingerClient.py
+++ UDPPingerClient/UDPPingerClient/UDPPingerClient.py
@@ -1,24 +1,29 @@
 ﻿# UDPPingerClient
 # By: Team 7
 
-from socket import *
+import socket
+from socket import AF_INET, SOCK_DGRAM
 import time
+
 
 #Create Socket
 serverName = '127.0.0.1' #Local Host Default
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket = socket.socket(AF_INET, SOCK_DGRAM)
+clientSocket.settimeout(1)
 
-for i in range(1,10): #loop 10 times
+for i in range(1,11): #loop 10 times
     #Send ping/Start Timer
     start = time.time()
     clientSocket.sendto(bytes('ping', 'utf-8'), (serverName, serverPort))
 
     #Recieve Response/End Timer
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-    end = time.time()
-
-    #Print Response (or Timeout Message)
-    print(modifiedMessage, i, start-end)
-
+    try:
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+        end = time.time()
+        print(modifiedMessage.decode('utf-8'), i, end-start)
+    except socket.timeout:
+        end = time.time()
+        print('Request timed out', i, end-start)
+    
 clientSocket.close()
