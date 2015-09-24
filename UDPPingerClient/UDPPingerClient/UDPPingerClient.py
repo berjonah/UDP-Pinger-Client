c0fc17805d4a40bd1a11ede70e576e6dde3100c4
Nathan Emerson <nemerson@nnu.edu>
2015-09-24 13:26:37 -0600

    Changed floating point precision Added group memebers' names to top comment

--- UDPPingerClient/UDPPingerClient/UDPPingerClient.py
+++ UDPPingerClient/UDPPingerClient/UDPPingerClient.py
@@ -1,5 +1,5 @@
 ﻿# UDPPingerClient
-# By: Team 7
+# By: Team 7 (Andrew Fillmore, Colby Gerdes, Nathan Emerson)
 
 import socket
 from socket import AF_INET, SOCK_DGRAM
@@ -9,10 +9,10 @@ sum = 0.0
 min = 2.0
 max = 0.0
 numSuccessPing = 0
-total = 1000
+total = 10
 
 #Create Socket
-serverName = '127.0.0.1' #Local Host Default
+serverName = '10.209.0.21' #Local Host Default
 serverPort = 12000
 clientSocket = socket.socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(1)
@@ -28,7 +28,7 @@ for i in range(1,total + 1): #loop 10 times
         modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
         end = time.time()
         rtt = end - start
-        print(modifiedMessage.decode('utf-8'), i, float("{0:.10f}".format(rtt)))
+        print(modifiedMessage.decode('utf-8'), i, float("{0:.5f}".format(rtt)))
         sum += rtt
 
         #Set min
@@ -42,9 +42,9 @@ for i in range(1,total + 1): #loop 10 times
     except socket.timeout:
         print('Request timed out', i)
 
-print('\nMin =', float("{0:.10f}".format(min)),
-      ' Max =', float("{0:.10f}".format(max)),
-      ' Average =', float("{0:.10f}".format(sum / numSuccessPing)))
+print('\nMin =', float("{0:.5f}".format(min)),
+      ' Max =', float("{0:.5f}".format(max)),
+      ' Average =', float("{0:.5f}".format(sum / numSuccessPing)))
 
 print('Lost =', ((total - numSuccessPing) / total) * 100,'%')
 
